2b632367b932dfe522d448214f2eaedabc2b822a
Raihan Febriyansah <raihanfebriyansahh@gmail.com>
2024-08-21 23:52:17 +0700

    [A][routes][feat]: routes untuk user, role dan login

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,11 @@
 
 use App\Http\Controllers\Administrator\ClientController;
 use App\Http\Controllers\Administrator\DashboardController;
+use App\Http\Controllers\Administrator\Auth\LoginController;
+use App\Http\Controllers\Administrator\RoleController;
 use App\Http\Controllers\Administrator\ServicesController;
 use App\Http\Controllers\Administrator\SliderController;
+use App\Http\Controllers\Administrator\UserController;
 use App\Http\Controllers\FrontendController;
 use Illuminate\Support\Facades\Route;
 
@@ -13,9 +16,14 @@ Route::get('/services', [FrontendController::class, 'services'])->name('services
 Route::get('/contact', [FrontendController::class, 'contact'])->name('contact');
 Route::get('/show', [FrontendController::class, 'show'])->name('show');
 
+Route::get('/login', [LoginController::class, 'index'])->name('login');
+Route::post('/login', [LoginController::class, 'authenticate'])->name('authenticate');
+
 Route::prefix('administrator')->group(function () {
     route::resource('dashboard', DashboardController::class);
     route::resource('slider', SliderController::class);
     route::resource('client', ClientController::class);
     route::resource('service', ServicesController::class);
+    route::resource('user', UserController::class);
+    route::resource('role', RoleController::class);
 });
